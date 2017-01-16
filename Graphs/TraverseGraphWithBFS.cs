2c9b848e520c490cab8b297b04899b1757f821a8
georgi8706 <georgi_vz@abv.bg>
2017-01-17 01:48:20 +0200

    Added iterative BFS algorithm.

--- Graphs/TraverseGraphWithBFS.cs
+++ Graphs/TraverseGraphWithBFS.cs
@@ -12,11 +12,12 @@ namespace Algorithms
         List<int> _outputNodes;
         List<int> _visitedNodes;
 
+
         public void Run()
         {
             var graph = new Graph();
 
-            graph.AddChildNodes(0, new List<int>() { 3, 6});
+            graph.AddChildNodes(0, new List<int>() { 3, 6 });
             graph.AddChildNodes(1, new List<int>() { 2, 3, 4, 5, 6 });
             graph.AddChildNodes(2, new List<int>() { 1, 4, 5 });
             graph.AddChildNodes(3, new List<int>() { 1, 5 });
@@ -26,41 +27,72 @@ namespace Algorithms
 
             var nodeValues = new string[] { "Ruse", "Sofia", "Pleven", "Varna", "Bourgas", "Stara Zagora", "Plovdiv" };
 
-            TraverseGraphBFS(graph, nodeValues);
+            TraverseGraphBFS(graph, nodeValues, false);
         }
 
-        private void TraverseGraphBFS(Graph graph, string[] nodeValues)
+        /// <summary>
+        /// Traverses all the nodes of the graph.
+        /// </summary>
+        private void TraverseGraphBFS(Graph graph, string[] nodeValues, bool useReqursiveImplementation)
         {
             _graph = graph;
             _outputNodes = new List<int>();
             _visitedNodes = new List<int>();
 
-            // Start from 0 node children
-            //for (int node = 0; node < 2; node++)
-            //{
+            if (useReqursiveImplementation)
+            {
+                _visitedNodes.Add(4);
+                _outputNodes.Add(4);
+
                 Console.Write("Start BFS from " + nodeValues[4] + ": ");
                 TraverseGraphNodeBFS(4);
-                 
-                // Map the node id(s) with the value
-                foreach (var n in _outputNodes)
-                { 
-                    Console.Write(nodeValues[n] + ", ");
-                }
+            }
+            else
+            {
+                // Iterative implementation
+                TraverseGraphBFS(4);
+            }
 
-                _outputNodes.Clear();
-                Console.WriteLine();
-            //}
+            // Map the node id(s) with the value
+            foreach (var n in _outputNodes)
+            {
+                Console.Write(nodeValues[n] + ", ");
+            }
+
+            Console.WriteLine();
+        }
+
+        private void TraverseGraphBFS(int node)
+        {
+            var nodes = new Queue<int>();
+            nodes.Enqueue(node);
+
+            int currNode = node;
+
+            while (nodes.Count > 0)
+            {
+                node = nodes.Dequeue();
+
+                if (!_visitedNodes.Contains(node))
+                {
+                    _visitedNodes.Add(node);
+                    _outputNodes.Add(node);
+
+                    var children = _graph[node];
+
+                    if (children != null)
+                    {
+                        foreach (var child in children)
+                        {
+                            nodes.Enqueue(child);
+                        }
+                    }
+                }
+            }
         }
 
         private void TraverseGraphNodeBFS(int node)
         {
-            if (_visitedNodes.Contains(node))
-            {
-                return;
-            }
-
-            _visitedNodes.Add();
-
             var children = _graph[node];
 
             if (children == null)
@@ -68,12 +100,40 @@ namespace Algorithms
                 return;
             }
 
-            _outputNodes.AddRange(children);
-            _visitedNodes.AddRange(children);
+            var visitedChildred = new bool[children.Count];
 
+            int i = 0;
             foreach (var child in children)
             {
-                TraverseGraphNodeBFS(child);
+                if (!_visitedNodes.Contains(child))
+                {
+                    _visitedNodes.Add(child);
+                    _outputNodes.Add(child);
+                    visitedChildred[i] = false;
+                }
+                else
+                {
+                    visitedChildred[i] = true;
+                }
+
+                i++;
+            }
+
+            i = 0;
+            foreach (var child in children)
+            {
+                if (!visitedChildred[i])
+                {
+                    _visitedNodes.Add(child);
+                    TraverseGraphNodeBFS(child);
+                }
+
+                i++;
+            }
+
+            if (_visitedNodes.Contains(node))
+            {
+                return;
             }
         }
     }
